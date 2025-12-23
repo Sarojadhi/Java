32704f0a903ad9548b2611e45ba076a91e115bfd
Saroj Adhikari <sarojadh1111@gmail.com>
2025-12-23 08:17:12 +0545

    class

---
+++ ClassObjExmpl.java
@@ -0,0 +1,15 @@
+class Test {
+    int a = 10 ; String b = "Saroj";
+    void show()
+    {
+        System.out.println(a+" "+b);
+    }
+}
+
+public class ClassObjExmpl {
+    public static void main(String[] args) {
+        
+    Test r = new Test();
+        r.show();
+    }
+}
