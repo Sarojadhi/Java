67e3d0162187073db0b81e8fa1d5d9e3118c2330
Saroj Adhikari <sarojadh1111@gmail.com>
2026-01-06 20:00:52 +0545

    thread runnable

--- Thread.java
+++ Thread.java
@@ -1,4 +1,4 @@
-class MyThread extends Thread {
+/* class MyThread extends Thread {
     public void run() {
         for (int i = 1; i <= 5; i++) {
             System.out.println("Thread: " + i);
@@ -10,5 +10,25 @@ public class Main {
     public static void main(String[] args) {
         MyThread t = new MyThread();
         t.start(); 
+    }
+}
+ */
+
+
+
+//  Runnable Interface 
+
+class MyTask implements Runnable {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Runnable: " + i);
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Thread t = new Thread(new MyTask());
+        t.start();
     }
 }
