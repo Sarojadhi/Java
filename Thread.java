a99d6c60c9f4c5773cd900b1394c81714a47afb2
Saroj Adhikari <sarojadh1111@gmail.com>
2026-01-06 20:06:13 +0545

    thred runSym

--- Thread.java
+++ Thread.java
@@ -17,7 +17,7 @@ public class Main {
 
 
 //  Runnable Interface 
-
+/* 
 class MyTask implements Runnable {
     public void run() {
         for (int i = 1; i <= 5; i++) {
@@ -30,5 +30,34 @@ public class Main {
     public static void main(String[] args) {
         Thread t = new Thread(new MyTask());
         t.start();
+    }
+} */
+
+
+// T wo thread running Symultenoudsly 
+class Task1 extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Task 1 - " + i);
+        }
+    }
+}
+
+class Task2 extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Task 2 - " + i);
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Task1 t1 = new Task1();
+        Task2 t2 = new Task2();
+
+        t1.start();   // Thread 1
+        t2.start();   // Thread 2
     }
 }
