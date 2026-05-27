4aefc63ed942913bce406c11a3433eabe5fc9c18
Saroj <168215255+Sarojadhi@users.noreply.github.com>
2026-05-27 20:40:50 +0545

    conversion

---
+++ conversion.java
@@ -0,0 +1,10 @@
+public class conversion {
+    public static void main(String args[]) {
+        double d = 3.5;
+        long l = (long) d;
+        int i = (int) l;
+        System.out.println("Double value: " + d);
+        System.out.println("Long value: " + l);
+        System.out.println("Integer value: " + i);
+    }
+}
