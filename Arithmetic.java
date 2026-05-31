bc3511f5474d0b2c90a813a040d850d344edf37c
Saroj <168215255+Sarojadhi@users.noreply.github.com>
2026-05-31 22:41:42 +0545

    Arithmetic

---
+++ Arithmetic.java
@@ -0,0 +1,11 @@
+package unit2;
+
+public class Arithmetic {
+    public static void main(String args[]) {
+        int a = 2, b = 3;
+        System.out.println("sum=" + (a + b));
+        System.out.println("difference=" + (a - b));
+        System.out.println("product=" + (a * b));
+
+    }
+}
